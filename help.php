50347a85a6813e388a602c9e695f7d040d5582cd
Sorin <test@test.com>
2017-02-01 21:52:13 -0800

    removed html header in help.php

--- help.php
+++ help.php
@@ -6,8 +6,7 @@ require '../include/library.php';
 
 //userLogin();
 
-// html header
-require('../include/header.php');
+// removed html header
 ?>
 <form>
 <div class="title"><img class="menu4" src="res/help.png" alt="" />Help</div><p>
