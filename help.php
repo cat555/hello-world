68bc0e3e7ce15eaa4b2f89519ca8ec44c94c8d09
Sorin <test@test.com>
2017-02-01 22:10:47 -0800

    sorin commit

--- help.php
+++ help.php
@@ -6,8 +6,8 @@ require '../include/library.php';
 
 //userLogin();
 
-// modified html header
-require('../include/header_modified.php');
+// sorin is here
+require('../include/sorin_is_here.php');
 ?>
 <form>
 <div class="title"><img class="menu4" src="res/help.png" alt="" />Help</div><p>
